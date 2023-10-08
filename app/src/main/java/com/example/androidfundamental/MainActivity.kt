4d186f60c9aefa2051339d50ea63fa32fc9cfc3d
rayarsandani <rayarsandani@gmail.com>
2023-10-08 23:34:23 +0700

    done

---
+++ app/src/main/java/com/example/androidfundamental/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.androidfundamental
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var tv : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tv = findViewById(R.id.textView)
+
+        val username = intent.getParcelableExtra<User>("User")?.username
+        val password = intent.getParcelableExtra<User>("User")?.password
+        tv.text = "Username: $username dan Password : $password"
+
+
+        val btnImplicit : Button = findViewById(R.id.btn_implicit)
+        btnImplicit.setOnClickListener(this)
+
+        val btnfragment : Button = findViewById(R.id.btn_fragment)
+        btnfragment.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btn_implicit ->{
+//                val message = "Hallo tes"
+//                val intent = Intent()
+//                intent.action = Intent.ACTION_SEND
+//                intent.putExtra(Intent.EXTRA_TEXT, message)
+//                intent.type = "text/plain"
+//                startActivity(intent)
+                val intent = Intent()
+                intent.putExtra("history","Anda sudah login")
+                setResult(RESULT_OK, intent)
+                finish()
+
+
+            }
+            R.id.btn_fragment ->{
+                val intent = Intent(this@MainActivity,MainActivity2::class.java)
+                startActivity(intent)
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/androidfundamental/MainActivity2.kt
@@ -0,0 +1,42 @@
+package com.example.androidfundamental
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.androidfundamental.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+
+    lateinit var binding : ActivityMain2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnFragment1.setOnClickListener {
+
+            replaceFragment(Fragment1())
+        }
+
+        binding.btnFragment2.setOnClickListener {
+
+            replaceFragment(Fragment2())
+
+        }
+
+
+
+    }
+
+    private fun replaceFragment (fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer,fragment)
+        fragmentTransaction.commit()
+
+    }
+
+}
+
